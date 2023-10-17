c1b6565f811f208feec865a5326e4042082abaaa
Stuart Saint <stuartsaint242@gmail.com>
2023-10-17 21:34:47 +0100

    q5 done

--- labs/lab02.py
+++ labs/lab02.py
@@ -6,10 +6,18 @@ url = "http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML"
 page = req.get(url)
 doc = x.parseString(page.content)
 
+# print(doc.toprettyxml())
+
 # writes data to a file. If the file does not exist, it is created
 with open("currenttrains.xml","w") as xml_file:
     doc.writexml(xml_file)
 
-parsed = x.parse(xml_file)
-
 trainList = doc.getElementsByTagName("objTrainPositions")
+
+for train in trainList:
+    codeNode = train.getElementsByTagName("TrainCode").item(0)
+    latNode = train.getElementsByTagName("TrainLatitude").item(0)
+    trainCode = codeNode.firstChild.nodeValue.strip()
+    trainLat = latNode.firstChild.nodeValue.strip()
+    print(f'Train Code: {trainCode}, Lat: {trainLat}')
+
