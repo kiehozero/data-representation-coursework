220abe10090f92e2b84d601f3c6c0f79b1a33567
Stuart Saint <stuartsaint242@gmail.com>
2023-12-10 22:03:15 +0000

    formatting

--- labs/lab02.py
+++ labs/lab02.py
@@ -8,12 +8,13 @@ page = req.get(url)
 doc = x.parseString(page.content)
 
 # A list of tags contained in the XML data, to be used later
-tagArr = ['TrainCode','TrainStatus','TrainDate','Direction','TrainLatitude','TrainLongitude','PublicMessage']
+tagArr = ['TrainCode', 'TrainStatus', 'TrainDate', 'Direction',
+          'TrainLatitude', 'TrainLongitude', 'PublicMessage']
 
 # print(doc.toprettyxml())
 
 # Writes data to a file. If the file does not exist, it is created
-with open("currenttrains.xml","w") as xml_file:
+with open("currenttrains.xml", "w") as xml_file:
     doc.writexml(xml_file)
 
 trainList = doc.getElementsByTagName("objTrainPositions")
@@ -25,15 +26,21 @@ for train in trainList:
     trainCode = codeNode.firstChild.nodeValue.strip()
     trainLat = latNode.firstChild.nodeValue.strip()
 
-# Combine the previous with and for loops to create a CSV 
-with open('currenttrains.csv', mode='w',newline='') as train_csv:
-    train_writer = csv.writer(train_csv, delimiter='\t', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+# Combine the previous with and for loops to create a CSV
+with open('currenttrains.csv', mode='w', newline='') as train_csv:
+    train_writer = csv.writer(train_csv, delimiter='\t', quotechar='"',
+                              quoting=csv.QUOTE_MINIMAL)
     objTrainPositionsNodes = doc.getElementsByTagName("objTrainPositions")
     for node in objTrainPositionsNodes:
         dataList = []
         for tag in tagArr:
             trainNode = node.getElementsByTagName(tag).item(0)
-            tagData =  trainNode.firstChild.nodeValue.strip()
+            tagData = trainNode.firstChild.nodeValue.strip()
             dataList.append(tagData)
         train_writer.writerow(dataList)
 
+# Read the CSV file and print the contents to the console
+with open('currenttrains.csv', newline='') as train_csv:
+    train_reader = csv.reader(train_csv, delimiter='\t', quotechar='"')
+    for row in train_reader:
+        print(', '.join(row))
